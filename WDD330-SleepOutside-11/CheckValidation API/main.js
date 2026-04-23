bf9467f65d7763dc3f185c5f21d11f97ab666c37
Your Name <your@email.com>
2026-04-23 08:42:00 -0500

    main

---
+++ WDD330-SleepOutside-11/CheckValidation API/main.js
@@ -0,0 +1,17 @@
+const fs = document.getElementById("fs");
+
+function checkFileSize() {
+  const files = fs.files;
+
+  // If there is (at least) one file selected
+  if (files.length > 0) {
+    if (files[0].size > 75 * 1000) {
+      // Check the constraint
+      fs.setCustomValidity("The selected file must not be larger than 75 kB");
+      fs.reportValidity();
+      return;
+    }
+  }
+  // No custom constraint violation
+  fs.setCustomValidity("");
+}
