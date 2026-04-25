d85fbf19d09480ffda8d3e03af3941b043698a4e
Your Name <your@email.com>
2026-04-25 16:56:53 -0500

    index

--- WDD330-SleepOutside-11/CheckValidation API/main.js
+++ WDD330-SleepOutside-11/CheckValidation API/main.js
@@ -1,17 +1,18 @@
 const fs = document.getElementById("fs");
 
 function checkFileSize() {
-  const files = fs.files;
+    const files = fs.files;
 
-  // If there is (at least) one file selected
-  if (files.length > 0) {
-    if (files[0].size > 75 * 1000) {
-      // Check the constraint
-      fs.setCustomValidity("The selected file must not be larger than 75 kB");
-      fs.reportValidity();
-      return;
+    // If there is (at least) one file selected
+    if (files.length > 0) {
+        if (files[0].size > 75 * 1000) {
+            // Check the constraint
+            fs.setCustomValidity("The selected file must not be larger than 75kB");
+            fs.checkValidity();
+            return;
+        }
     }
-  }
-  // No custom constraint violation
-  fs.setCustomValidity("");
+
+    // No custom constraint violation
+    fs.setCustomValidity("");
 }
